c79ff6304dd03ca97224ace47c02e98fb22ca746
coly <User@colys-Mac.local>
2013-05-22 14:21:55 +0800

    Initial Commit

---
+++ qiushibaike/colyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  colyAppDelegate.h
+//  qiushibaike
+//
+//  Created by coly on 13-5-22.
+//  Copyright (c) 2013年 coly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface colyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ qiushibaike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  qiushibaike
+//
+//  Created by coly on 13-5-22.
+//  Copyright (c) 2013年 coly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "colyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([colyAppDelegate class]));
+    }
+}
